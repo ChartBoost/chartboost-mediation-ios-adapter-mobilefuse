8e9ccb0203c3375672dce6337ded519290ec383a
Daniel Barros <daniel.barros@chartboost.com>
2024-04-25 10:02:58 -0700

    Relocate adapter properties to configuration

--- Source/MobileFuseAdapter.swift
+++ Source/MobileFuseAdapter.swift
@@ -15,18 +15,26 @@ final class MobileFuseAdapter: PartnerAdapter {
     // MARK: PartnerAdapter
 
     /// The version of the partner SDK.
-    let partnerSDKVersion = MobileFuse.version() ?? ""
+    var partnerSDKVersion: String {
+        MobileFuseAdapterConfiguration.partnerSDKVersion
+    }
 
     /// The version of the adapter.
     /// It should have either 5 or 6 digits separated by periods, where the first digit is Chartboost Mediation SDK's major version, the last digit is the adapter's build version, and intermediate digits are the partner SDK's version.
     /// Format: `<Chartboost Mediation major version>.<Partner major version>.<Partner minor version>.<Partner patch version>.<Partner build version>.<Adapter build version>` where `.<Partner build version>` is optional.
-    let adapterVersion = "4.1.7.0.0"
+    var adapterVersion: String {
+        MobileFuseAdapterConfiguration.adapterVersion
+    }
 
     /// The partner's unique identifier.
-    let partnerID = "mobilefuse"
+    var partnerID: String {
+        MobileFuseAdapterConfiguration.partnerID
+    }
 
     /// The human-friendly partner name.
-    let partnerDisplayName = "MobileFuse"
+    var partnerDisplayName: String {
+        MobileFuseAdapterConfiguration.partnerDisplayName
+    }
 
     /// The designated initializer for the adapter.
     /// Chartboost Mediation SDK will use this constructor to create instances of conforming types.
